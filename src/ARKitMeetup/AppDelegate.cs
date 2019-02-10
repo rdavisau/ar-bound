27208d57f039e4ebdf98371f1d9776101a5470d0
Ryan Davis <rdavis@guildlink.com.au>
2019-02-10 13:58:52 +1000

    Add support for hot reload

--- src/ARKitMeetup/AppDelegate.cs
+++ src/ARKitMeetup/AppDelegate.cs
@@ -1,4 +1,9 @@
-﻿using ARKitMeetup.Helpers;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using ARKitMeetup.Helpers;
 using Foundation;
 using UIKit;
 
@@ -11,6 +16,7 @@ namespace ARKitMeetup
 
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
+            ListIPAddresses();
 #if DEBUG
             new Continuous.Server.HttpServer().Run();
 #endif
@@ -21,6 +27,23 @@ namespace ARKitMeetup
             Window.MakeKeyAndVisible();
             
             return true;
+        }
+
+        private void ListIPAddresses()
+        {        
+            try
+            {
+                var inet = 
+                    NetworkInterface
+                        .GetAllNetworkInterfaces()
+                        .SelectMany(x => 
+                            x.GetIPProperties()
+                             .UnicastAddresses.Where(y => y.Address.AddressFamily == AddressFamily.InterNetwork))
+                        .Select(y => y.Address);
+
+                Debug.WriteLine(String.Join(Environment.NewLine, inet));
+                
+            } catch (Exception ex) { Debug.WriteLine(ex); }
         }
     }
 }
---
+++ src/ARKitMeetup/Helpers/WH.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ARKitMeetup.Helpers
+{
+    public static class WH
+    {
+        private static Dictionary<string, object> items = new Dictionary<string, object>();
+
+        public static T GetOrSet<T>(string key, T set)
+        {
+            object item;
+            if (items.TryGetValue(key, out item))
+                return (T)item;
+
+            items[key] = set;
+
+            return set;
+        }
+    }
+}
